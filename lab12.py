af7b52e9ff88f61d957df75e1697762c87141357
y-a-r-i-k <gusevyaroslaveggg666@gmail.com>
2025-04-05 13:11:28 +0300

    lab12 added

---
+++ lab12.py
@@ -0,0 +1,25 @@
+#variant 2
+
+vowels = set('aeiouy')
+numbers = set('1234567890')
+
+#Задаем текст вручную
+text1 = "QWERTYUIOPLKHGFDSAZXCVBNM1234567890" #Тут точно больше согласных
+text2 = "AEIOUYBCD12387234"                   #Тут точно больше гласных
+text3 = "abce1234"                            #Кол-во равно
+
+def compare_items(text):
+    number_vowels = sum(1 for x in text if x in vowels)
+    number_others = sum(1 for x in text if x not in vowels)
+    number_numbers = sum(1 for x in text if x in numbers)
+
+    if number_vowels > number_others - number_numbers:
+        print("Гласных больше")
+    elif number_vowels < number_others - number_numbers:
+        print("Согласных больше")
+    else:
+        print("Количества равны")
+
+compare_items(text1)
+compare_items(text2)
+compare_items(text3)
