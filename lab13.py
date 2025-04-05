6b535dfe69c7e7a04686b03585d10dbe83d02fd1
y-a-r-i-k <gusevyaroslaveggg666@gmail.com>
2025-04-05 13:26:31 +0300

    lab 13 added

---
+++ lab13.py
@@ -0,0 +1,42 @@
+#variant 2
+
+text = "В этом тексте есть числа 123, 4567 и 89, а также 12345."
+
+#Умное решение
+# import re #Регулярные выражения никто не запрещеал
+
+
+
+# def find_longest_digit_sequence(text):
+#     digit_sequences = re.findall(r'\d+', text) #Находим все последовательности цифр в тексте
+    
+#     if digit_sequences:
+#         longest_sequence = max(digit_sequences, key=len)
+#         return longest_sequence
+#     else:
+#         return None
+
+
+def find_longest_digit_sequence(text):
+    max_sequence = ""
+    current_sequence = ""
+
+    for char in text:
+        if char.isdigit():  
+            current_sequence += char  
+        else:
+            if len(current_sequence) > len(max_sequence):
+                max_sequence = current_sequence
+            current_sequence = ""  
+
+    if len(current_sequence) > len(max_sequence):
+        max_sequence = current_sequence
+
+    return max_sequence
+
+
+longest_sequence = find_longest_digit_sequence(text)
+if longest_sequence:
+    print(f"Максимальная последовательность цифр: {longest_sequence}")
+else:
+    print("Не найденно.")
