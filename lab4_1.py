e4355d7bcd297abdda1e7a6ea08c9085406c5d8a
y-a-r-i-k <gusevyaroslaveggg666@gmail.com>
2025-04-23 12:21:50 +0300

    lab4_1 done

--- lab4_1.py
+++ lab4_1.py
@@ -1,17 +1,15 @@
 #variant 1
 def f(x):
     if -2 <= x <= -1:
-        return 2 * x + 4
-    if -1 < x <= 1:
-        return 2 * (x ** 2)
-    if 1 < x <= 2:
-        return -2 * x + 2
-    if x < -2 or x > 2:
-        return None #Undef function
+        res = 2 * x + 4
+    elif -1 < x <= 1:
+        res = 2 * (x ** 2)
+    elif 1 < x <= 2:
+        res = -2 * x + 2
+    else:
+        res = "Функция не определена"
+    print(res)
 
-def main():
-    print(f(int(input())))
+inp = int(input())
+f(inp)
 
-
-if __name__ == "__main__":
-    main()
