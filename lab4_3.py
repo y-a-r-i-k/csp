686a8a2f3d8376fbc47fdefb377d6cb684d86e89
y-a-r-i-k <gusevyaroslaveggg666@gmail.com>
2025-04-23 12:32:07 +0300

    lab4_3 done

--- lab4_3.py
+++ lab4_3.py
@@ -2,16 +2,16 @@
 
 def check(x, y, r_in, r_out, x1, y1) -> bool:
     if ((x - x1) ** 2 + (y - y1) ** 2 >= r_in ** 2) and ((x - x1) ** 2 + (y - y1) ** 2 <= r_out ** 2):
-        return True
-    return False
+        res = True
+    else:
+        res = False
+    return res
     
 
-def main():
-    x, y = map(int, input("(x, y): ").split())
-    r_in, r_out = map(int, input("(r, R): ").split())
-    x1, y1 = map(int, input("(x1, y1): ").split())
+x, y = map(int, input("(x, y): ").split())
+r_in, r_out = map(int, input("(r, R): ").split())
+x1, y1 = map(int, input("(x1, y1): ").split())
 
-    print(check(x, y, r_in, r_out, x1, y1))
+res = check(x, y, r_in, r_out, x1, y1)
+print(res)
 
-if __name__ == "__main__":
-    main()
