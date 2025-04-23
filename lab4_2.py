fed12f948cad062a19d936ca97ab30133fd3bac0
y-a-r-i-k <gusevyaroslaveggg666@gmail.com>
2025-04-23 12:15:16 +0300

    lab4_1 done

--- lab4_2.py
+++ lab4_2.py
@@ -1,12 +1,12 @@
 #variant 1
-
 def mymax(a, b):
-    return a if a > b else b
+    if a > b:
+        m = a
+    else:
+        m = b
+    return m
 
-def main():
-    x1, x2, x3, x4 = map(int, input().split())
-    print(mymax(mymax(x1, x2), mymax(x3, x4)))
-
-
-if __name__ == "__main__":
-    main()
+x1, x2, x3, x4 = map(int, input().split())
+a = mymax(x1, x2)
+b = mymax(x3, x4)
+print(mymax(a, b))
