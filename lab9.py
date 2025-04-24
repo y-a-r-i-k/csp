bfdda01a0d3c394cd37eb65eefa3a8de405503d2
y-a-r-i-k <gusevyaroslaveggg666@gmail.com>
2025-04-24 12:29:57 +0300

    lab9

--- lab9.py
+++ lab9.py
@@ -1,13 +1,31 @@
-#variant 9
+#variant 1 - переделать
 
-def cout_negative(l: list) -> int:
-    return sum(1 for x in l if x < 0)
+def cnt_neg(l: list) -> int:
+    cnt = 0
+    for i in range(len(l)):
+        if l[i] < 0:
+            cnt += 1
+    return cnt
 
-def count_positive(l: list) -> int:
-    return sum(1 for x in l if x > 0)
+def cnt_pos(l: list) -> int:
+    cnt = 0
+    for i in range(len(l)):
+        if l[i] > 0:
+            cnt += 1
+    return cnt
 
-def count_zeros(l: list) -> int:
-    return sum(1 for x in l if x == 0)
+def cnt_zer(l: list) -> int:
+    cnt = 0
+    for i in range(len(l)):
+        if l[i] == 0:
+            cnt += 1
+    return cnt
 
 l = [-2, -1, 0, 1, 2]
-print(f"Отрицательных: {cout_negative(l)}; Положительных: {count_positive(l)}; Нулевых: {count_zeros(l)}")
+#print(f"Отрицательных: {cnt_neg(l)}; Положительных: {cnt_pos(l)}; Нулевых: {cnt_zer(l)}")
+
+neg = cnt_neg(l)
+pos = cnt_pos(l)
+zer = cnt_zer(l)
+
+print(neg, pos, zer)
